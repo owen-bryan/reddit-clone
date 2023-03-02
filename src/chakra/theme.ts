949d93872a19a45b0343f74a1c9e2be1e69c18e5
owen-bryan <odog654@gmail.com>
2023-03-02 02:00:26 -0500

    Began adding to navbar

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,4 +1,7 @@
-import {extendTheme} from "@chakra-ui/react"
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import {extendTheme} from "@chakra-ui/react";
 
 export const theme = extendTheme ({
     colors: {
@@ -8,5 +11,15 @@ export const theme = extendTheme ({
     },
     fonts: {
         body : "Open Sans, sans-serif",
+    },
+    styles: {
+        global : () => ({
+            body : {
+                bg: "gray.200",
+            },
+        }),
+    },
+    components: {
+        
     },
 });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 // import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react/dist/chakra-provider'
-import type { AppProps } from 'next/app'
-import {theme} from '../chakra/theme'
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import {theme} from '../chakra/theme';
+import Layout from '../components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps}/>
+      <Layout>
+        <Component {...pageProps}/>
+      </Layout>
     </ChakraProvider>
   )
-}
+};
