8439f3afcdb39e219f275e151411eb0d40f8192f
owen-bryan <odog654@gmail.com>
2023-03-02 01:21:03 -0500

    added fonts

---
+++ src/chakra/theme.ts
@@ -0,0 +1,12 @@
+import {extendTheme} from "@chakra-ui/react"
+
+export const theme = extendTheme ({
+    colors: {
+        brand : {
+            100 : "#FF3c00",
+        },
+    },
+    fonts: {
+        body : "Open Sans, sans-serif",
+    },
+});
