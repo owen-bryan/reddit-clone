21a0a22d8f70bf15db970517c402252662e390e1
owen-bryan <odog654@gmail.com>
2023-03-04 01:24:11 -0500

    started firebase section.

--- src/components/Modal/Auth/AuthInputs.tsx
+++ src/components/Modal/Auth/AuthInputs.tsx
@@ -3,6 +3,7 @@ import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import Login from './Login';
+import SignUp from './SignUp';
 
 type AuthInputsProps = {
     
@@ -18,7 +19,7 @@ const AuthInputs:React.FC<AuthInputsProps> = () => {
             mt={4} 
         >
             {modalState.view === "login" && <Login />}
-            {/* {modalState.view === "signup" && <SignUp />} */}
+            {modalState.view === "signup" && <SignUp />}
         </Flex>
     );
 };
--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,8 +1,9 @@
 import { authModalState } from '@/atoms/authModalAtom';
-import { Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from '@chakra-ui/react';
+import { Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useRecoilState } from "recoil";
 import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
 
 const AuthModal:React.FC = () => {
     const [modalState, setModalState] = useRecoilState(authModalState);
@@ -38,7 +39,8 @@ const AuthModal:React.FC = () => {
                             justify="center" 
                             width="70%"
                         >
-                            {/* <OauthButtons /> */}
+                            <OAuthButtons />
+                            <Text color="gray.500" fontWeight={700}>OR</Text>
                             <AuthInputs />
                             {/* <ResetPassword /> */}
                         </Flex>
