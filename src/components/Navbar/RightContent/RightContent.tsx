484b1e29909f7d8ca9daa826f8f0a2cb1ad5854c
owen-bryan <odog654@gmail.com>
2023-03-05 00:06:52 -0500

    Directories started

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,12 +1,14 @@
 import AuthModal from '@/components/Modal/Auth/AuthModal';
 import { auth } from '@/firebase/clientApp';
 import { Flex, Button } from '@chakra-ui/react';
-import { signOut } from 'firebase/auth';
+import { signOut, User } from 'firebase/auth';
 import React from 'react';
 import AuthButtons from './AuthButtons';
+import Icons from './Icons';
+import UserMenu from './UserMenu';
 
 type RightContentProps = {
-    user: any;
+    user?: User | null;
 };
 
 const RightContent:React.FC<RightContentProps> = ({ user }) => {
@@ -16,7 +18,8 @@ const RightContent:React.FC<RightContentProps> = ({ user }) => {
             <AuthModal />
  
             <Flex justify="center" align="center">
-                {user ? <Button onClick={() => signOut(auth)}>Logout</Button> : <AuthButtons />}
+                {user ? <Icons /> : <AuthButtons />}
+                <UserMenu  user={user}/>
             </Flex>
         </>    
     )
