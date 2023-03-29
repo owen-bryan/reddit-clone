e241d8b4952e765e7c53e4468d3342361fba4646
owen-bryan <odog654@gmail.com>
2023-03-29 02:07:39 -0400

    Finished the new posts form.

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -5,17 +5,16 @@ type PageContentProps = {};
 
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
   return (
-    <Flex justify="center" p="16px 0px" border="1px solid red">
+    <Flex justify="center" p="16px 0px">
       <Flex
         width="95%"
         justify="center"
         maxWidth="860px"
-        border="1px solid green"
       >
         {/* LHS */}
-        <Flex direction="column" width={{base: "100%", md: "65%"}} mr={{base: 0, md:6}} border="1px solid blue">{children[0]}</Flex>
+        <Flex direction="column" width={{base: "100%", md: "65%"}} mr={{base: 0, md:6}} >{children[0]}</Flex>
         {/* RHS */}
-        <Flex direction="column" display={{ base: "none", md: "flex"}} flexGrow={1} border="1px solid orange">{children[1]}</Flex>
+        <Flex direction="column" display={{ base: "none", md: "flex"}} flexGrow={1} >{children[1]}</Flex>
       </Flex>
     </Flex>
   );
---
+++ src/components/Posts/NewPostForm.tsx
@@ -0,0 +1,82 @@
+import { Flex, Icon } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { IoDocumentText, IoImageOutline } from "react-icons/io5";
+import { BiPoll } from "react-icons/bi";
+import { BsMic } from "react-icons/bs";
+import TabItem from "./TabItem";
+import TextInputs from "./TextInputs";
+
+type NewPostFormProps = {};
+
+const formTabs = [
+  {
+    title: "Post",
+    icon: IoDocumentText,
+  },
+  {
+    title: "Images & Video",
+    icon: IoImageOutline,
+  },
+  {
+    title: "Poll",
+    icon: BiPoll,
+  },
+  {
+    title: "Talk",
+    icon: BsMic,
+  },
+];
+
+export type TabItem = {
+  title: string;
+  icon: typeof Icon.arguments;
+};
+
+const NewPostForm: React.FC<NewPostFormProps> = () => {
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  const [textInputs, setTextInputs] = useState({
+    title: "",
+    body: "",
+  });
+  const [seelctedFile, setSelectedFile] = useState<string>();
+  const [loading, setLoading] = useState(false);
+  const handleCreatePost = async () => {};
+  const onSelectImage = () => {};
+  const onTextChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { name, value },
+    } = event;
+    setTextInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <Flex direction="column" bg="white" borderRadius={4} mt={2}>
+      <Flex width="100%">
+        {formTabs.map((item) => (
+          <TabItem
+            key={item.title}
+            item={item}
+            selected={item.title === selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
+        ))}
+      </Flex>
+      <Flex p={4}>
+        {selectedTab === "Post" && (
+          <TextInputs
+            textInputs={textInputs}
+            handleCreatePost={handleCreatePost}
+            onChange={onTextChange}
+            loading={loading}
+          />
+        )}
+      </Flex>
+    </Flex>
+  );
+};
+export default NewPostForm;
