f067759d7aafd0490037316c55e13fcfbccb2c3e
owen-bryan <odog654@gmail.com>
2023-04-06 02:57:08 -0400

    Added comments

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,10 +1,12 @@
 import { Post } from "@/atoms/postsAtom";
 import About from "@/components/Community/About";
 import PageContent from "@/components/Layout/PageContent";
+import Comments from "@/components/Posts/Comments/Comments";
 import PostItem from "@/components/Posts/PostItem";
 import { auth, firestore } from "@/firebase/clientApp";
 import useCommunityData from "@/hooks/useCommunityData";
 import usePosts from "@/hooks/usePosts";
+import { User } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
@@ -16,14 +18,14 @@ const PostsPage: React.FC = () => {
     usePosts();
   const router = useRouter();
   const { communityStateValue } = useCommunityData();
-  
+
   const fetchPost = async (postId: string) => {
     try {
       const postDocRef = doc(firestore, "posts", postId);
       const postDoc = await getDoc(postDocRef);
       setPostStateValue((prev) => ({
         ...prev,
-        selectedPost: { id: postDoc.id, ...postDoc.data () } as Post,
+        selectedPost: { id: postDoc.id, ...postDoc.data() } as Post,
       }));
     } catch (error: any) {
       console.log("fetchPost erorr error", error.message);
@@ -31,7 +33,6 @@ const PostsPage: React.FC = () => {
   };
 
   useEffect(() => {
-    
     const { pid } = router.query;
 
     if (pid && !postStateValue.selectedPost) {
@@ -55,7 +56,11 @@ const PostsPage: React.FC = () => {
             userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId}
           />
         )}
-        {/* comments */}
+        <Comments
+          user={user as User}
+          selectedPost={postStateValue.selectedPost}
+          communityId={postStateValue.selectedPost?.communityId as string}
+        />
       </>
       <>
         {communityStateValue.currentCommunity && (
