d3d313e9f320ff6920371ba3b84ad068ad27eadd
owen-bryan <odog654@gmail.com>
2023-04-08 17:33:16 -0400

    Finished the directory menu.

--- src/components/Navbar/Directory/MenuListItem.tsx
+++ src/components/Navbar/Directory/MenuListItem.tsx
@@ -1,3 +1,4 @@
+import useDirectory from "@/hooks/useDirectory";
 import { Flex, Icon, Image, MenuItem } from "@chakra-ui/react";
 import React from "react";
 import { IconType } from "react-icons/lib";
@@ -17,16 +18,24 @@ const MenuListItem: React.FC<MenuListItemProps> = ({
   iconColor,
   imageURL,
 }) => {
+  const { onSelectMenuItem } = useDirectory();
   return (
-    <MenuItem width="100%" fontSize="10pt" _hover={{ bg: "gray.100" }} onClick={() => {}}>
-        <Flex align="center">
-            {imageURL ? (
-                <Image src={imageURL} borderRadius="full" boxSize="18px" mr={2} />
-            ) : (
-                <Icon as={icon} fontSize={20} mr={2} color={iconColor} />
-            )}
-            {displayText}
-        </Flex>
+    <MenuItem
+      width="100%"
+      fontSize="10pt"
+      _hover={{ bg: "gray.100" }}
+      onClick={() =>
+        onSelectMenuItem({ displayText, link, icon, iconColor, imageURL })
+      }
+    >
+      <Flex align="center">
+        {imageURL ? (
+          <Image src={imageURL} borderRadius="full" boxSize="18px" mr={2} />
+        ) : (
+          <Icon as={icon} fontSize={20} mr={2} color={iconColor} />
+        )}
+        {displayText}
+      </Flex>
     </MenuItem>
   );
 };
